093019be5bbb5303922f7fb7cd8c87beddd7501e
Carlos Castro <alu0100819847@ull.edu.es>
2017-03-26 17:33:44 +0100

    Incluida clase Celsius

--- Temperature.js
+++ Temperature.js
@@ -44,3 +44,25 @@ class Kelvin extends Temperatura {
     return this.resultado
   }
 }
+
+class Celsius extends Temperatura {
+  constructor(temp){
+      super(temp);
+  }
+
+  convertir() {
+    this.resultado = 0;
+    if(super.getCover.match(/[Cc]/)){
+      this.resultado = super.getValor;
+    }
+    else{
+      if(super.getCover.match(/[Ff]/)){
+        this.resultado = (super.getValor * (9/5))+32;
+      }
+      else{
+        this.resultado = super.getValor - 0 + 273.15;
+      }
+    }
+    return this.resultado;
+  }
+}
