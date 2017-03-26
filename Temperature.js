025ca27647cfada244aa7b5d0ced64374f5ba7ac
Carlos Castro <alu0100819847@ull.edu.es>
2017-03-26 17:41:15 +0100

    Añadido metodo type a medida

--- Temperature.js
+++ Temperature.js
@@ -7,6 +7,28 @@ class Medida {
     if(array[2]){ this.tipo = array[2]; }
     if(array[3]){ this.cover = array[3]; }
   }
+
+  static type(temp){
+    var array = temp.match(/([-+]?\d+(?:\.\d+)?)\s*([FfCcKkMmPp])\s*(?:to)?\s*([FfCcKkMmPp])\s*$/);
+    var typ = 0;
+    if(array != null){
+      if(array[2].match(/[FfCcKkMmPp]/) && array[3].match(/[FfCcKkMmPp]/)) {
+        if(array[2].match(/[FfCcKk]/)) {
+          typ = 1;
+        }
+        else {
+          typ = 2;
+        }
+      }
+      else {
+        console.log("Expresión inválida.")
+      }
+    }
+    else{
+      console.log("Expresión inválida.")
+    }
+    return typ;
+  }
   get getValor() { return this.valor }
   get getTipo() { return this.tipo }
   get getCover() { return this.cover }
